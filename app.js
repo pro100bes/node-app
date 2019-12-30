208007093dbb9249b5e2d7fff6a50bbc36336ef0
Max <beskorovainyi.max@gmail.com>
2019-12-30 13:01:14 +0200

    basic commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    '/javascripts',
+    express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+module.exports = app;
